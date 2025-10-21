ec0533b45f92617de0f61177162066047ed435af
ribmen <v.g.menezes100@gmail.com>
2025-10-21 10:31:45 -0300

    fix (V1): eureka discovery service correctly implemented

--- user/src/main/java/com/docchain/user/infrastructure/config/DocumentApiClientConfig.java
+++ user/src/main/java/com/docchain/user/infrastructure/config/DocumentApiClientConfig.java
@@ -2,26 +2,35 @@ package com.docchain.user.infrastructure.config;
 
 import com.docchain.user.infrastructure.http.client.DocumentApiClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class DocumentApiClientConfig {
 
-    @Value("http://localhost:8081")
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Value("${document.service.url:lb://document-service}")
     private String documentServiceUrl;
 
     @Bean
-    public DocumentApiClient documentServiceClient() {
-        RestClient restClient = RestClient.builder()
+    public DocumentApiClient documentServiceClient(WebClient.Builder builder) {
+        WebClient client = builder
                 .baseUrl(documentServiceUrl)
                 .build();
 
         HttpServiceProxyFactory factory = HttpServiceProxyFactory
-                .builderFor(RestClientAdapter.create(restClient))
+                .builderFor(WebClientAdapter.create(client))
                 .build();
 
         return factory.createClient(DocumentApiClient.class);
